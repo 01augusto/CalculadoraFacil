2a7a72f0c44544bd8770a013c0254a36bfb8a461
maq01 <augusto_iurko@hotmail.com>
2026-03-02 22:17:20 -0300

    Firt Commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,37 +1,29 @@
 package org.example
 
 fun main() {
-    println("Digite a expressão (ex: 5 + 7):")
-    val input = readLine() ?: return
+    println("Digite sua Operação")
+    val op = readln().trim().replace(" ", "")
 
-    val regex = Regex("""\s*(-?\d+(?:\.\d+)?)\s*([+\-*/])\s*(-?\d+(?:\.\d+)?)\s*""")
-    val match = regex.matchEntire(input)
-
-    if (match != null) {
-        val (n1, operator, n2) = match.destructured
-        val a = n1.toDouble()
-        val b = n2.toDouble()
-
-        val result = when (operator) {
-            "+" -> a + b
-            "-" -> a - b
-            "*" -> a * b
-            "/" -> if (b != 0.0) a / b else {
-                println("Erro: divisão por zero")
-                return
-            }
-            else -> {
-                println("Operador inválido")
-                return
-            }
-        }
-
-        if (result % 1.0 == 0.0) {
-            println(result.toInt())
-        } else {
-            println(result)
-        }
-    } else {
-        println("Expressão inválida")
+    if (op.contains("+")){
+        val soma = op.split("+")
+        val s = (soma[0].toInt() + soma[1].toInt())
+        println(s)
+    } else if(op.contains("*")) {
+        val soma = op.split("*")
+        val s = (soma[0].toInt() * soma[1].toInt())
+        println(s)
+    } else if (op.contains("/")){
+        val soma = op.split("/")
+        val s = (soma[0].toInt() / soma[1].toInt())
+        println(s)
+    } else if (op.contains("-")){
+        val soma = op.split("-")
+        val s = (soma[0].toInt() - soma[1].toInt())
+        println(s)
+    }else{
+        println("Operação Não existente")
     }
+
+
+
 }
